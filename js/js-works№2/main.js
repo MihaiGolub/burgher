62d6f4c878374b8c32e97c6dcd6085fec1c12d67
MihaiGolub <mihai.goluov@gmail.com>
2018-11-25 01:11:37 +0200

    15.11

---
+++ js/js-works№2/main.js
@@ -0,0 +1,6 @@
+var input = [4, 9, 86, 55, 40];
+var than = [23, 12, 2, 8, 27];
+var result = ['элементы из input больше than']
+ function filter(input, than) {
+ 	if ()
+}
--- main.js
+++ main.js
@@ -11,5 +11,15 @@ let menuClose = document.getElementById('mobilemenuClose');
 	menu.classList.remove('mobile-menu--open');
 });
 
+let btnMore = document.getElementById('more');
+let closeMore = document.getElementById('peoplePage');
 
+btnMore.addEventListener('click', function(listen){
+	listen.preventDefault();
+	let coment = document.getElementById('pComent');
+	coment.classList.add('people__comment_active');
+});
 
+btnMore.addEventListener('click', function(listen){
+	listen.preventDefault();
+});
